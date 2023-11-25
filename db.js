62eaf5acecc341f0f0ffe2c021560572974f81c4
priyanshusharman <priyanshuk1900@gmaul.com>
2023-11-25 20:03:01 +0530

    online data link

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://0.0.0.0:27017/doormonk";
+const mongoURI = "mongodb+srv://Doormonk:Tleklu0wPvqOjuSG@doormonk.ajlmam6.mongodb.net/";
+// const mongoURI = "mongodb://0.0.0.0:27017/doormonk";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
